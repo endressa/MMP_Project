6a28acc5ad0a5f9b65c62794b0cbc2c3c6d9fd39
VincentUllal <85555471+VincentUllal@users.noreply.github.com>
2021-06-15 11:41:51 +0200

    mini bug fix

--- GreatGame/Assets/Scripts/DisappearingBlock.cs
+++ GreatGame/Assets/Scripts/DisappearingBlock.cs
@@ -10,6 +10,16 @@ public class DisappearingBlock : MonoBehaviour
     void Start()
     {
         myPosition = transform.position;    //save the position of the DisappearingBlock
+        if (visible)
+            {
+            gameObject.transform.SetPositionAndRotation(new Vector3(1000, 1000, 1000), new Quaternion());   //just tp it far away lol
+            Debug.Log("bye");
+            visible = !visible;
+            }else{
+                transform.position = myPosition;    //set the position to the saved Position
+                visible = !visible;
+                Debug.Log("hey");
+            }
     }
 
     // Update is called once per frame
